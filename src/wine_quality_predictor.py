44aa0317b136ff64f1e4c8da30142a80a0651b8b
idona-cell <idona@stevens.edu>
2026-05-06 06:17:01 -0400

    Update wine_quality_predictor.py

--- src/wine_quality_predictor.py
+++ src/wine_quality_predictor.py
@@ -4,4 +4,12 @@ class WinePredictor:
 """
 This uses the WineQualityAnanlyzer class from the analyzer function to predict the quality of a wine based on a user input or recommend a wine based on a user input of different quality standards.
 """
-def __init
+def __init__(self, analyzer: WineQualityAnalyzer):
+  if not isinstance(analyzer, WineQualityAnalyzer):
+    raise TypeError("The analyzer must be an object in WineQualityAnalyzer. try again.")
+
+self.analyzer = analyzer
+self.data = analyzer.data
+
+def predict_quality(self, user_features):
+  
