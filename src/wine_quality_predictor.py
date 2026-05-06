994ef449d23040f94889ce766595879ba62bf02f
idona-cell <idona@stevens.edu>
2026-05-06 06:14:44 -0400

    Update and rename wine_quality_predictor to wine_quality_predictor.py

---
+++ src/wine_quality_predictor.py
@@ -0,0 +1,7 @@
+from src.wine_analyzer import WineQualityAnalyzer
+
+class WinePredictor:
+"""
+This uses the WineQualityAnanlyzer class from the analyzer function to predict the quality of a wine based on a user input or recommend a wine based on a user input of different quality standards.
+"""
+def __init
